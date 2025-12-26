bd90700b928720202adcaf5add372dae36e33ad0
ellusagency <ellus.agency.adm@gmail.com>
2025-12-26 14:54:34 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 // servir frontend
 app.use(express.static(path.join(__dirname, "../public")));
 
-// suas rotas
+// rotas
 app.use("/generate-image", require("./routes/generateImage"));
 
 // fallback para SPA
@@ -17,20 +17,3 @@ app.get("*", (req, res) => {
 });
 
 module.exports = app;
-
-
-
-/*const express = require("express");
-const path = require("path");
-const generateImageRoute = require("./routes/generateImage");
-
-const app = express();
-
-app.use(express.json());
-
-// 👇 ESSA LINHA É A NOVA
-app.use(express.static(path.join(__dirname, "../public")));
-
-app.use("/generate-image", generateImageRoute);
-
-module.exports = app;*/
