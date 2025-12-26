1fba8b1cbb74624ae0ca2edd82570efad300712b
ellusagency <ellus.agency.adm@gmail.com>
2025-12-26 14:54:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = require("./src/app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
 
 
